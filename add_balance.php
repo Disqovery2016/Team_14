1fc6491ced6d3486c89e0a5bca0a90b8ebe1126b
ashutoshsjoshi <13bit023@nirmauni.ac.in>
2016-11-04 17:01:11 +0530

    Updation of Seller Database for Balance

--- add_balance.php
+++ add_balance.php
@@ -14,9 +14,15 @@ $x = +$_GET['weight']*$y;
 $sql = "INSERT INTO seller_transaction (mobile, weight, type, amount)
 VALUES ('".$_GET['user']."', ".$_GET['weight'].", '".$_GET['type']."', ".$x.")";
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "New record created successfully ";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$sql = "UPDATE seller SET balance = balance + ".$x." WHERE phone=".$_GET['user'];
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully ";
+} else {
+    echo "Error updating record: " . $conn->error;
 }
 
 $conn->close();
